7963a17408009d13474d67a40f09aeb3d1636ba1
David Evans <david0evans@gmail.com>
2013-06-01 15:04:00 -0700

    Added more input handling to TopDownMovement component

--- components/TopDownMovement.js
+++ components/TopDownMovement.js
@@ -4,7 +4,11 @@ TankJS.addComponent("TopDownMovement")
 
 .initFunction(function()
 {
-  this.velocity = [0, 0];
+  this.left = false;
+  this.up = false;
+  this.right = false;
+  this.down = false;
+  this.movementSpeed = 50;
 
   TankJS.addEventListener("OnKeyPressed", this);
   TankJS.addEventListener("OnKeyReleased", this);
@@ -20,18 +24,37 @@ TankJS.addComponent("TopDownMovement")
 
 .addFunction("OnKeyPressed", function(key)
 {
-  console.log("press");
-  this.velocity[0] = 10;
+  if (key == TankJS.W)
+    this.up = true;
+  else if (key == TankJS.A)
+    this.left = true;
+  else if (key == TankJS.S)
+    this.down = true;
+  else if (key == TankJS.D)
+    this.right = true;
 })
 
 .addFunction("OnKeyReleased", function(key)
 {
-  this.velocity[0] = 0;
+  if (key == TankJS.W)
+    this.up = false;
+  else if (key == TankJS.A)
+    this.left = false;
+  else if (key == TankJS.S)
+    this.down = false;
+  else if (key == TankJS.D)
+    this.right = false;
 })
 
 .addFunction("OnEnterFrame", function(dt)
 {
   var t = this.parent.getComponent("2D");
-  t.x += this.velocity[0] * dt;
-  t.y += this.velocity[1] * dt;
+  if (this.left)
+    t.x -= this.movementSpeed * dt;
+  if (this.up)
+    t.y -= this.movementSpeed * dt;
+  if (this.right)
+    t.x += this.movementSpeed * dt;
+  if (this.down)
+    t.y += this.movementSpeed * dt;
 });
