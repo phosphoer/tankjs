5a70a36e7967c15d3a5c24efe64a000edfe6a900
David Evans <david0evans@gmail.com>
2014-02-16 15:57:02 -0800

    Update RenderManager

--- components/RenderManager.js
+++ components/RenderManager.js
@@ -3,6 +3,9 @@ TANK.registerComponent("RenderManager")
 .construct(function ()
 {
   this.context = null;
+  this.camera = {x: 0, y: 0, z: 1};
+  this.clearColor = "#000";
+  this.nearestNeighbor = true;
   this._drawables = {};
   this._drawablesSorted = [];
 })
@@ -25,15 +28,34 @@ TANK.registerComponent("RenderManager")
     this._drawables[existing[i].name + existing[i].parent.id] = existing[i];
   this.sort();
 
+
   this.update = function (dt)
   {
     if (!this.context)
       return;
 
+    // Nearest neighbor
+    if (this.nearestNeighbor)
+    {
+      this.context.imageSmoothingEnabled = false;
+      this.context.webkitImageSmoothingEnabled = false;
+      this.context.mozImageSmoothingEnabled = false;
+    }
+
+    this.context.save();
+    this.context.fillStyle = this.clearColor;
+    this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+    this.context.restore();
+
+    this.context.save();
+    this.context.translate(this.context.canvas.width / 2, this.context.canvas.height / 2);
+    this.context.scale(1 / this.camera.z, 1 / this.camera.z);
+    this.context.translate(-this.context.canvas.width / 2, -this.context.canvas.height / 2);
     for (var i in this._drawablesSorted)
     {
-      this._drawablesSorted[i].draw(this.context);
+      this._drawablesSorted[i].draw(this.context, this.camera, dt);
     }
+    this.context.restore();
   };
 
   this.addEventListener("OnComponentInitialized", function (c)
