63aa8a818910313a7d230943e3978a4ce7d3a38f
David Evans <david0evans@gmail.com>
2014-04-06 22:02:54 -0700

    Add input keycode defines

---
+++ src/Keys.js
@@ -0,0 +1,66 @@
+(function(TANK)
+{
+  "use strict";
+
+  TANK.Key = {};
+
+  // Mouse buttons treated as "key"
+  TANK.Key.LEFT_MOUSE = 0;
+  TANK.Key.MIDDLE_MOUSE = 1;
+  TANK.Key.RIGHT_MOUSE = 2;
+
+  // Special keys
+  TANK.Key.LEFT_ARROW = 37;
+  TANK.Key.UP_ARROW = 38;
+  TANK.Key.RIGHT_ARROW = 39;
+  TANK.Key.DOWN_ARROW = 40;
+  TANK.Key.SHIFT = 16;
+  TANK.Key.BACKSPACE = 8;
+  TANK.Key.ESCAPE = 27;
+  TANK.Key.SPACE = 32;
+  TANK.Key.CONTROL = 17;
+  TANK.Key.ALT = 18;
+  TANK.Key.SUPER = 91;
+  TANK.Key.TILDE = 192;
+
+  // Letters
+  TANK.Key.A = 65;
+  TANK.Key.B = 66;
+  TANK.Key.C = 67;
+  TANK.Key.D = 68;
+  TANK.Key.E = 69;
+  TANK.Key.F = 70;
+  TANK.Key.G = 71;
+  TANK.Key.H = 72;
+  TANK.Key.I = 73;
+  TANK.Key.J = 74;
+  TANK.Key.K = 75;
+  TANK.Key.L = 76;
+  TANK.Key.M = 77;
+  TANK.Key.N = 78;
+  TANK.Key.O = 79;
+  TANK.Key.P = 80;
+  TANK.Key.Q = 81;
+  TANK.Key.R = 82;
+  TANK.Key.S = 83;
+  TANK.Key.T = 84;
+  TANK.Key.U = 85;
+  TANK.Key.V = 86;
+  TANK.Key.W = 87;
+  TANK.Key.X = 88;
+  TANK.Key.Y = 89;
+  TANK.Key.Z = 90;
+
+  // Number keys (not numpad)
+  TANK.Key.NUM1 = 49;
+  TANK.Key.NUM2 = 50;
+  TANK.Key.NUM3 = 51;
+  TANK.Key.NUM4 = 52;
+  TANK.Key.NUM5 = 53;
+  TANK.Key.NUM6 = 54;
+  TANK.Key.NUM7 = 55;
+  TANK.Key.NUM8 = 56;
+  TANK.Key.NUM9 = 57;
+  TANK.Key.NUM0 = 48;
+
+})(this.TANK = this.TANK || {});
