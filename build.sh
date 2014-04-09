cddd7d4f1664a1288a0b714c9096f6261f6c7c4a
David Evans <david0evans@gmail.com>
2014-04-09 00:24:28 -0700

    Remove old files

--- build.sh
+++
@@ -1,1 +0,0 @@
-uglifyjs js/addProperty.js js/requestAnimFrame.js js/Math.js js/KeyCodes.js js/Component.js js/Entity.js js/Space.js js/Engine.js -o Tank.min.js
--- components/DeleteOnCollide.js
+++
@@ -1,9 +0,0 @@
-TANK.registerComponent("DeleteOnCollide")
-
-.initialize(function ()
-{
-  this.OnCollide = function (obj)
-  {
-    this.space.removeEntity(this.parent);
-  };
-});
