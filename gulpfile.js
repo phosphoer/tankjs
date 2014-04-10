270499a2d6d704a62496b5dbdbdddbe3bdbb3f76
David Evans <david0evans@gmail.com>
2014-04-09 18:02:38 -0700

    Add uglify to build

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,28 @@
 
   var gulp = require("gulp");
   var concat = require("gulp-concat");
+  var uglify = require("gulp-uglify");
+  var rename = require("gulp-rename");
 
   // Main build task
   gulp.task("default", function()
   {
     gulp.src("src/*.js")
     .pipe(concat("tank.js"))
+    .pipe(gulp.dest("bin/"))
+    .pipe(uglify())
+    .pipe(rename("tank.min.js"))
     .pipe(gulp.dest("bin/"));
   });
 
+  // Component build task
   gulp.task("components", function()
   {
     gulp.src("components/*.js")
     .pipe(concat("components.js"))
+    .pipe(gulp.dest("bin/"))
+    .pipe(uglify())
+    .pipe(rename("components.min.js"))
     .pipe(gulp.dest("bin/"));
   });
 
