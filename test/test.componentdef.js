3bc79652ff5f869128b93e5d63de38def93cf173
David Evans <david0evans@gmail.com>
2014-04-11 18:04:29 -0700

    Add tests to ComponentDef

--- test/test.componentdef.js
+++ test/test.componentdef.js
@@ -23,6 +23,50 @@
         expect(c._includes).to.be.a.instanceOf(Array);
         expect(c._includes).to.contain("A");
       });
+
+      it("should recursively add included Components, handling circular references", function()
+      {
+        TANK.registerComponent("IncludeTestC").includes("IncludeTestD");
+        TANK.registerComponent("IncludeTestD").includes(["IncludeTestE", "IncludeTestF"]);
+        TANK.registerComponent("IncludeTestE").includes("IncludeTestC");
+        TANK.registerComponent("IncludeTestF");
+        var e = TANK.createEntity("IncludeTestC");
+        expect(e).to.have.property("IncludeTestC");
+        expect(e).to.have.property("IncludeTestD");
+        expect(e).to.have.property("IncludeTestE");
+        expect(e).to.have.property("IncludeTestF");
+      });
     });
+
+    describe("construct()", function()
+    {
+      it("should set the _construct function", function()
+      {
+        var func = function() {};
+        var c = new TANK.ComponentDef("ComponentDefTest").construct(func);
+        expect(c._construct).to.equal(func);
+      });
+    });
+
+    describe("initialize()", function()
+    {
+      it("should set the _initialize function", function()
+      {
+        var func = function() {};
+        var c = new TANK.ComponentDef("ComponentDefTest").initialize(func);
+        expect(c._initialize).to.equal(func);
+      });
+    });
+
+    describe("uninitialize()", function()
+    {
+      it("should set the _uninitialize function", function()
+      {
+        var func = function() {};
+        var c = new TANK.ComponentDef("ComponentDefTest").uninitialize(func);
+        expect(c._uninitialize).to.equal(func);
+      });
+    });
+
   });
 })();
